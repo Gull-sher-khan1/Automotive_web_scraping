5d8f1ed202b9d1010b339328686c655673fe6e81
Gull Sher Khan <gullsherkhan1@gmail.com>
2024-07-11 18:12:53 +0100

    AWS-2: Cleaned data using pylint

--- Automotive_web_scraping/spiders/news.py
+++ Automotive_web_scraping/spiders/news.py
@@ -1,25 +1,27 @@
-import scrapy
+"""News Spider"""
+# pylint: disable=abstract-method
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from ..utils.data_utils import FormatData
 
 class NewsSpider(CrawlSpider):
+    """Generic spider class for scraping news on websites"""
     name = "news"
     start_urls = []
     allowed_domains = []
 
-
-    def __init__(self, site = None, *a, **kw):
+    def __init__(self, *a, site = None, **kw):
         super().__init__(*a, **kw)
-        if(site is None): return
-
-        self.data = FormatData(self.name).get_data(site)
-        self.start_urls = map(lambda path: f"https://{site + path}", self.data['startPath'])
-        self.rules = tuple(map(lambda rl: (Rule(LinkExtractor(allow=f"{rl}"), callback="parse_item", follow=True)), self.data['allow']))
-        self.allowed_domains = [site]
-        self.data = self.data['format']
+        if site is not None:
+            self.data = FormatData(self.name).get_data(site)
+            self.start_urls = map(lambda path: f"https://{site + path}", self.data['startPath'])
+            self.rules = tuple(map(lambda rl: (Rule(LinkExtractor(allow=f"{rl}"),\
+                                                    callback="parse_item",\
+                                                    follow=True)), self.data['allow']))
+            self.allowed_domains = [site]
+            self.data = self.data['format']
 
     def parse_item(self, response):
-        item = {}
-        
+        """Parses scrapped data from website"""
+        item = {response}
         return item
--- Automotive_web_scraping/utils/data_utils.py
+++ Automotive_web_scraping/utils/data_utils.py
@@ -1,13 +1,16 @@
+"""Contains utilities used through out the project"""
 import json
 
 class FormatData:
+    """Class for formating the data obtained from config.json"""
     def __init__(self, name):
         self.data_file = f"{name}.json"
         self.data = self._load_data()
 
     def _load_data(self):
-        with open(self.data_file, "r") as f:
+        with open(self.data_file, "r", encoding="utf-8") as f:
             return json.load(f)
 
     def get_data(self, site):
+        """Returns the data needed for formating related to a site"""
         return self.data[site]
