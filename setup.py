d04b553aaf1c3a39f375d8627955004d25178f59
Gull Sher Khan <gullsherkhan1@gmail.com>
2024-08-02 00:44:09 +0100

    Issue with setup.py fixed

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'project',
-    version      = '1.0',
+    version      = '2.0',
     packages     = find_packages(),
     scripts      = ['run.py'],
     package_data = {
         'project': ['configurations/*.json']
     },
     entry_points = {'scrapy': ['settings = Automotive_web_scraping.settings']},
+    include_package_data=True,
 )
